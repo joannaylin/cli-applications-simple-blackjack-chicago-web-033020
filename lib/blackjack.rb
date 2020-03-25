1c6e0564a77f927cd2bab56e588d2cb7c958bb7f
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-25 02:46:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,15 +39,21 @@ end
 
 def hit?(card_total)
   # code hit? here
+  valid_inputs = ["h", "s"]
+
   prompt_user
   user_input = get_user_input
-  if user_input == 'h'
-    card_total += deal_card
-  elsif user_input == 's'
-    card_total
-  else
+
+  until valid_inputs.include?(user_input)
     invalid_command
-    hit?(card_total)
+    prompt_user
+    user_input = get_user_input
+  end
+
+  if user_input == "h"
+    card_total += deal_card
+  end
+  card_total
 end
 
 def invalid_command
